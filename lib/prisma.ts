66bab74652941bf432ccb4deda565c0c99677751
yassienE4 <yassienebrahim@gmail.com>
2026-01-27 22:41:35 +0200

    set up prisma and db

---
+++ lib/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
